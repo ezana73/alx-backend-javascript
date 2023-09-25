5e63429a352898e1cbb524f08ee14c2cb1fcf1b3
ezana73 <ezama49@gmail.com>
2023-09-25 14:10:40 +0000

    1-block-scoped.js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+xport default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
+    /* eslint-enable no-unused-vars */
+  }
+
+  return [task, task2];
+}
