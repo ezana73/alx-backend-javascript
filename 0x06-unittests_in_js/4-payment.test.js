5348e070b38a16f5ee2e040f5d2e2b72032ef52a
ezana73 <ezana49@gmail.com>
2023-11-22 18:18:48 +0000

    4-payment.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,4 +1,4 @@
-onst {describe, it} = require("mocha");
+const {describe, it} = require("mocha");
 const sinon = require("sinon");
 const sendPaymentRequestToApi = require("./4-payment");
 const Utils = require("./utils");
